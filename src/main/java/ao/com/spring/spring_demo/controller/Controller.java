e09d48121570526ca851b47044e7b1ae63be4488
Nicolau NP <mr1Normaliii@gmail.com>
2023-02-02 16:17:50 +0000

    trabalhando nos controllers

--- src/main/java/ao/com/spring/spring_demo/controller/Controller.java
+++ src/main/java/ao/com/spring/spring_demo/controller/Controller.java
@@ -1,5 +1,19 @@
 package ao.com.spring.spring_demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
-    
+
+    @GetMapping("")
+    public String mensagem() {
+        return "Hello world";
+    }
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Seja bem vindo";
+    }
+
 }
